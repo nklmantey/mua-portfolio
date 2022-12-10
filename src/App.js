7f9992aa0eaf012c9973b0734d59abb4c24e75aa
Nana Kofi Larbi Mantey <nklmantey@st.ug.edu.gh>
2022-12-10 12:36:37 +0000

    responsiveness

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Footer } from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
       <Banner />
       <Gallery />
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,22 +1,32 @@
-import { Container, Row } from "react-bootstrap";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import {Container, Row} from "react-bootstrap";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
   return (
-    <section className="banner" id="home">
+    <div className="banner" id="home">
       <Container>
-        <Row className="aligh-items-center">
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h1>{`OctobersBeauty`}</h1> 
-                <h1>{`Makeup Portfolio`}</h1> 
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-              </div>}
-            </TrackVisibility>
+        <Row className="align-items-center">
+          <TrackVisibility>
+            {({isVisible}) => (
+              <div
+                className={isVisible ? "animate__animated animate__fadeIn" : ""}
+              >
+                <h1>OctobersBeauty</h1>
+                <h1>Makeup</h1>
+                <h1>Portfolio</h1>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            )}
+          </TrackVisibility>
         </Row>
       </Container>
-    </section>
-  )
-}
+    </div>
+  );
+};
